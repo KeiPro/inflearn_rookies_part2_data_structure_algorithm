c00b06606dc82e90cddeda9080a132184e7dd54a
Kim youngwoo <keiprogramo@gmail.com>
2024-03-02 16:15:28 +0900

    Binary Tree 미로 알고리즘

--- Board.cs
+++ Board.cs
@@ -20,17 +20,62 @@ namespace Linear
 
         public void Initialize(int size)
         {
+            if (size % 2 == 0)
+                return;
+
             _tile = new TileType[size, size];
             _size = size;
 
-            for (int y = 0; y < _size; y++)
+            GenerateByBinaryTree(_size);
+
+        }
+
+        private void GenerateByBinaryTree(int size)
+        {
+            // 막는 작업 진행.
+            for (int y = 0; y < size; y++)
             {
-                for (int x = 0; x < _size; x++)
+                for (int x = 0; x < size; x++)
                 {
-                    if (x == 0 || x == size - 1 || y == 0 || y == size - 1)
+                    if (x % 2 == 0 || y % 2 == 0)
                         _tile[y, x] = TileType.Wall;
                     else
                         _tile[y, x] = TileType.Empty;
+                }
+            }
+
+            // 랜덤하게 길을 뚫는 작업 진행.
+            Random rand = new Random();
+            for (int y = 0; y < size; y++)
+            {
+                for (int x = 0; x < size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+
+                    if (x == size - 2 && y == size - 2)
+                        continue;
+
+                    if (y == size - 2)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (x == size - 2)
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                    }
+                    else
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                    }
                 }
             }
         }
